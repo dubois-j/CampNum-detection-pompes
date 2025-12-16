9543ecce33f1d0167baff93df255d141cb1b0d4d
Thibault Launois <thibault.launois@posteo.net>
2025-12-16 14:10:41 +0100

    refactor: implement module configuration

--- src/MediapipeProcessor.py
+++ src/MediapipeProcessor.py
@@ -1,10 +1,26 @@
 import mediapipe as mp
+#import PompeCount
+import cv2
 
-class MediaPipeProcessor():
+class MediaPipeProcessor:
     def __init__(self) -> None:
         Holistic = mp.solutions.holistic.Holistic
         self.model = Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        #self.PompeCount = PompeCount
+        self.camera = cv2.VideoCapture(0)
 
     def get_pose_world_landmarks(self, image):
         results = self.model.process(image)    
         return results.pose_world_landmarks
+    
+    def show_interface(self):
+        print("\nCOUCOU\n")
+        while self.camera.isOpened():
+            #count_pompe = self.PompeCount.getPompeCount
+            _, image = self.camera.read()
+            cv2.imshow('MediaPipe', image)
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                    break
+            
+        self.camera.release()
+        cv2.destroyAllWindows()
---
+++ src/__main__.py
@@ -0,0 +1,29 @@
+#import othello.board as Board
+#import othello.player as Player
+#from othello.othello import Othello
+#from othello.color import Color
+#from square import Square
+#from othello.ai_player_minimax import AIPlayerMiniMax
+from src.MediapipeProcessor import MediaPipeProcessor
+#import time
+
+process = MediaPipeProcessor()
+
+process.show_interface()
+
+#while not process.userDown():
+    
+
+
+#     game.board.updateBoard(game.currentPlayer.name, game.currentPlayer.color, showPossibleMoves=True, colorToShowMoves=game.currentPlayer.color)
+#     currentAvailableMoves = game.board.getPossibleMoves(game.currentPlayer.color)
+#     if len(currentAvailableMoves)>0: # Vérifier si ya des coups possibles
+#         (row,col)=game.currentPlayer.getMove(game.board.rownames,
+#                                        game.board.colnames,
+#                                        currentAvailableMoves)
+#         game.board.addPawn(row,col,game.currentPlayer.color)
+#         game.updateScore()
+#     game.nextPlayer()
+
+# game.board.updateBoard()
+# game.showResult()
